50c4afa397fd84e07479672c9dbff9bf973b7316
Viet Tung Le <viettung.le.2906@gmail.com>
2023-07-04 15:03:29 +0700

    Create models of response and video to parse JSON from API

---
+++ YoutubeClone/Models/Response.swift
@@ -0,0 +1,21 @@
+//
+//  Response.swift
+//  YoutubeClone
+//
+//  Created by Le Viet Tung on 04/07/2023.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    var items: [Video]
+    
+    enum CodingKeys: String, CodingKey {
+        case items
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+}
---
+++ YoutubeClone/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  YoutubeClone
+//
+//  Created by Le Viet Tung on 04/07/2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "AIzaSyAeWv5oHor5TFUcBW3Xq-c_uJEZMZJxfL8"
+    
+    static let API_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+}
