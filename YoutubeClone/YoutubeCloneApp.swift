fbe343b982472481c4a2400242b505e3f14bedb7
Viet Tung Le <viettung.le.2906@gmail.com>
2023-07-04 09:34:56 +0700

    Initial commit and add gitignore file

---
+++ YoutubeClone/YoutubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeCloneApp.swift
+//  YoutubeClone
+//
+//  Created by Le Viet Tung on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
